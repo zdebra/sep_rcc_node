ba612a93300136631cff7bbdcfa1fb8b0768a9b9
Zdenek <zd.brabec@gmail.com>
2017-01-05 14:58:04 +0100

    mock change requests

--- app/dbinit.js
+++ app/dbinit.js
@@ -24,6 +24,102 @@ const insertUsers = async function insertUsers(db) {
 
 }
 
+const insertChangeRequests = async function insertChangeRequests(db) {
+
+    const collection = db.collection('ChangeRequest')
+
+    const items = [
+        {
+            actionType: 'delete',
+            customerId: 2,
+            authorId: 2,
+            createdAt: new Date(),
+        },
+        {
+            actionType: 'update',
+            customerId: 1,
+            authorId: 1,
+            createdAt: new Date(),
+            data: {
+                createdAt: new Date(),
+                firstName: "Franta",
+                surname: "Vopršálek",
+                address: [
+                    {
+                        streetName: "Africká",
+                        streetNum: "160",
+                        postalCode: "160 00",
+                        cityPart: "Vokovice",
+                        city: "Praha",
+                        country: "CZ"
+                    },
+                    {
+                        streetName: "Africká 2",
+                        streetNum: "160",
+                        postalCode: "160 00",
+                        cityPart: "Vokovice",
+                        city: "Praha",
+                        country: "CZ"
+                    },
+                ],
+                phoneNumber: [
+                    {
+                        type: "mobile",
+                        number: "123456789",
+                        cityCode: "12",
+                        countryCode: "123"
+                    },
+                    {
+                        type: "mobile",
+                        number: "123456789",
+                    }
+                ],
+                birthNumber: "123456788",
+                countryOfOrigin: "CZ",
+            },
+
+        },
+        {
+            actionType: 'create',
+            authorId: 1,
+            createdAt: new Date(),
+            data: {
+                createdAt: new Date(),
+                firstName: "Pepa",
+                surname: "Suchac",
+                address: [
+                    {
+                        streetName: "Africká",
+                        streetNum: "160",
+                        postalCode: "160 00",
+                        cityPart: "Vokovice",
+                        city: "Praha",
+                        country: "CZ"
+                    }
+                ],
+                phoneNumber: [
+                    {
+                        type: "mobile",
+                        number: "13244214",
+                        cityCode: "12",
+                        countryCode: "123"
+                    },
+                    {
+                        type: "mobile",
+                        number: "123456789",
+                    }
+                ],
+                birthNumber: "213213",
+                countryOfOrigin: "US",
+            },
+
+        }
+    ]
+
+    return await collection.insertMany(items)
+
+}
+
 // Use connect method to connect to the server
 MongoClient.connect(process.env.MONGO_DB_URL, function(err, db) {
     console.log("Connected successfully to server");
